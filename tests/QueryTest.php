9d802e75ba69f701ca06ddd213655bd3b744c6b9
USAMI Kenta <tadsan@zonu.me>
2019-08-22 19:01:20 +0900

    Accept 0 as @int value

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -11,12 +11,14 @@ final class QueryTest extends \PHPUnit\Framework\TestCase
     public function test()
     {
         $pdo = new DummyPDO;
-        $orig = "string: :a@string\nint :b@int\nstring: :a@string\nint :b@int";
+        $orig = "string: :a@string\nint :b@int\nstring: :a@string\nint :b@int\nint :c1@int\nint :c1@int";
         $stmt = Query::build($pdo, $orig, [
             ':a' => 'AAAA',
             ':b' => '2222',
+            ':c1' => '0',
+            ':c2' => 0,
         ]);
-        $expected = 'string: @AAAA@ int 2222 string: @AAAA@ int 2222';
+        $expected = 'string: @AAAA@ int 2222 string: @AAAA@ int 2222 int 0 int 0';
         $this->assertSame($expected, $stmt->queryString);
     }
 }
