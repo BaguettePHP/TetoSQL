929a6e140323ef1382129d96caa437654f67b40c
USAMI Kenta <tadsan@zonu.me>
2022-05-18 01:02:45 +0900

    Fix type to PDOInterface and PDOStatementInterface

--- src/PDOInterface.php
+++ src/PDOInterface.php
@@ -1,25 +1,28 @@
 <?php
+
 namespace Teto\SQL;
 
 /**
  * Interface for PHP Data Objects (PDO) compatible class
  *
  * @copyright 1997-2016 the PHP Documentation Group
- * @license   http://php.net/manual/en/copyright.php CC-BY-3.0
- * @link      http://php.net/manual/class.pdo.php
+ * @license https://www.php.net/manual/en/copyright.php CC-BY-3.0
+ * @link https://www.php.net/manual/class.pdo.php
  *
  * ## Changes
  *
  * - Prototype declarations ware converted to a valid format as PHP code.
  * - Documents that have been taken from the PHP manual was converted to PHPDoc.
  * - Part of the variable name is substituted.
+ *
+ * @template T of \PDOStatement|PDOStatementInterface
  */
 interface PDOInterface
 {
     /**
      * Initiates a transaction
      *
-     * @link   http://php.net/manual/pdo.begintransaction.php
+     * @link https://www.php.net/manual/pdo.begintransaction.php
      * @return bool Returns TRUE on success or FALSE on failure.
      */
     public function beginTransaction();
@@ -27,7 +30,7 @@ interface PDOInterface
     /**
      * Commits a transaction
      *
-     * @link   http://php.net/manual/pdo.commit.php
+     * @link https://www.php.net/manual/pdo.commit.php
      * @return bool Returns TRUE on success or FALSE on failure.
      */
     public function commit();
@@ -35,7 +38,7 @@ interface PDOInterface
     /**
      * Fetch the SQLSTATE associated with the last operation on the database handle
      *
-     * @link   http://php.net/manual/pdo.errorcode.php
+     * @link https://www.php.net/manual/pdo.errorcode.php
      * @return string|null Returns an SQLSTATE, a five characters alphanumeric identifier defined in the ANSI SQL-92 standard.  Returns NULL if no operation has been run on the database handle.
      */
     public function errorCode();
@@ -43,15 +46,15 @@ interface PDOInterface
     /**
      * Fetch extended error information associated with the last operation on the database handle
      *
-     * @link   http://php.net/manual/pdo.errorinfo.php
-     * @return array Returns an array of error information about the last operation performed by this database handle.
+     * @link https://www.php.net/manual/pdo.errorinfo.php
+     * @return array<string,mixed> Returns an array of error information about the last operation performed by this database handle.
      */
     public function errorInfo();
 
     /**
      * Execute an SQL statement and return the number of affected rows
      *
-     * @link   http://php.net/manual/pdo.exec.php
+     * @link https://www.php.net/manual/pdo.exec.php
      * @param  string $statement The SQL statement to prepare and execute.
      * @return int Returns the number of rows that were modified or deleted by the SQL statement you issued.  If no rows were affected, returns 0.
      */
@@ -60,7 +63,7 @@ interface PDOInterface
     /**
      * Retrieve a database connection attribute
      *
-     * @link   http://php.net/manual/pdo.getattribute.php
+     * @link https://www.php.net/manual/pdo.getattribute.php
      * @param  int $attribute One of the PDO::ATTR_* constants.
      * @return mixed
      */
@@ -69,7 +72,7 @@ interface PDOInterface
     /**
      * Return an array of available PDO drivers
      *
-     * @link   http://php.net/manual/pdo.getavailabledrivers.php
+     * @link https://www.php.net/manual/pdo.getavailabledrivers.php
      * @return string[]
      */
     public static function getAvailableDrivers();
@@ -77,15 +80,15 @@ interface PDOInterface
     /**
      * Checks if inside a transaction
      *
-     * @link   http://php.net/manual/pdo.intransaction.php
-     * @return Returns TRUE if a transaction is currently active, and FALSE if not.
+     * @link https://www.php.net/manual/pdo.intransaction.php
+     * @return bool Returns TRUE if a transaction is currently active, and FALSE if not.
      */
     public function inTransaction();
 
     /**
      * Returns the ID of the last inserted row or sequence value
      *
-     * @link   http://php.net/manual/pdo.lastinsertid.php
+     * @link https://www.php.net/manual/pdo.lastinsertid.php
      * @param  string $name
      * @return string
      */
@@ -94,26 +97,28 @@ interface PDOInterface
     /**
      * Prepares a statement for execution and returns a statement object
      *
-     * @link   http://php.net/manual/pdo.prepare.php
-     * @param  string $statement      This must be a valid SQL statement template for the target database server.
-     * @param  array  $driver_options This array holds one or more key=>value pairs to set attribute values for the PDOStatement object that this method returns.
-     * @return \PDOStatement|false
+     * @link https://www.php.net/manual/pdo.prepare.php
+     * @param string $statement This must be a valid SQL statement template for the target database server.
+     * @param array<int,mixed> $driver_options This array holds one or more key=>value pairs to set attribute values for the PDOStatement object that this method returns.
+     * @return \PDOStatement|PDOStatementInterface|false
+     * @phpstan-return T|false
      */
-    public function prepare($statement, $driver_options = array());
+    public function prepare($statement, $driver_options = []);
 
     /**
      * Executes an SQL statement, returning a result set as a PDOStatement object
      *
-     * @link   http://php.net/manual/pdo.query.php
-     * @param  string The SQL statement to prepare and execute.
-     * @return \PDOStatement|false Returns a PDOStatement object, or FALSE on failure.
+     * @link http://php.net/manual/pdo.query.php
+     * @param string $statement The SQL statement to prepare and execute.
+     * @return \PDOStatement|PDOStatementInterface|false Returns a PDOStatement object, or FALSE on failure.
+     * @phpstan-return T|false
      */
     public function query($statement);
 
     /**
      * Quotes a string for use in a query
      *
-     * @link   http://php.net/manual/pdo.quote.php
+     * @link https://www.php.net/manual/pdo.quote.php
      * @param  string $string         The string to be quoted.
      * @param  int    $parameter_type Provides a data type hint for drivers that have alternate quoting styles.
      * @return string Returns a quoted string that is theoretically safe to pass into an SQL statement.  Returns FALSE if the driver does not support quoting in this way.
@@ -123,7 +128,7 @@ interface PDOInterface
     /**
      * Rolls back a transaction
      *
-     * @link   http://php.net/manual/pdo.rollback.php
+     * @link https://www.php.net/manual/pdo.rollback.php
      * @return bool Returns TRUE on success or FALSE on failure.
      */
     public function rollBack();
@@ -131,10 +136,10 @@ interface PDOInterface
     /**
      * Set an attribute
      *
-     * @link   http://php.net/manual/pdo.setattribute.php
+     * @link https://www.php.net/manual/pdo.setattribute.php
      * @param  int   $attribute One of the PDO::ATTR_* constants.
      * @param  mixed $value
-     * @return Returns TRUE on success or FALSE on failure.
+     * @return bool Returns TRUE on success or FALSE on failure.
      */
     public function setAttribute($attribute, $value);
 }
