82ff13d8f58803f0bd284e4a84c413f3af4e1870
USAMI Kenta <tadsan@zonu.me>
2022-05-18 01:05:02 +0900

    Fix type to PDOAggregate

--- src/PDOAggregate.php
+++ src/PDOAggregate.php
@@ -7,9 +7,14 @@ namespace Teto\SQL;
  * @copyright 2016 USAMI Kenta
  * @license   https://github.com/BaguettePHP/TetoSQL/blob/master/LICENSE MPL-2.0
  * @link      http://php.net/manual/class.pdo.php
+ * @template S of \PDOStatement|PDOStatementInterface
+ * @template T of \PDO|PDOInterface<S>
  */
 interface PDOAggregate
 {
-    /** @return \PDO|\Teto\SQL\PDOInterface */
+    /**
+     * @return \PDO|PDOInterface
+     * @phpstan-return T
+     */
     public function getPDO();
 }
