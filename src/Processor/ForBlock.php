795ee62772fef1429d7b587552a732b5a9b60c2d
USAMI Kenta <tadsan@zonu.me>
2022-05-21 04:02:03 +0900

    Impl Processor\ForBlock

---
+++ src/Processor/ForBlock.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Teto\SQL\Processor;
+
+use Teto\SQL\ProcessorInterface;
+
+use DomainException;
+
+/**
+ * Process `%for ... %endfor` block
+ *
+ * @copyright 2016 pixiv Inc.
+ * @license https://github.com/BaguettePHP/TetoSQL/blob/master/LICENSE MPL-2.0
+ */
+class ForBlock implements ProcessorInterface
+{
+    const FOR_PATTERN = '/
+^\s*%for\s*(?:\[(?<glue>[^\]]*)\])?\s+(?<name>:[a-zA-Z0-9_]+\s*$) # first line
+(?<block>\s[\s\S]*?\s) # block, includes %else
+^\s*%endfor$ # block termination
+/mx';
+
+    /** @var DynamicPlaceholder */
+    private $placeholder_replacer;
+
+    public function __construct(DynamicPlaceholder $placeholder_replacer)
+    {
+        $this->placeholder_replacer = $placeholder_replacer;
+    }
+
+    public function processQuery($pdo, $sql, array $params, array &$bind_values)
+    {
+        $built_sql = preg_replace_callback(self::FOR_PATTERN, function (array $matches) use (
+            $pdo, $params, &$bind_values
+        ) {
+            $glue = $matches['glue'];
+            $name = rtrim($matches['name']);
+            if (!isset($params[$name])) {
+                throw new DomainException(sprintf('Must be assigned parameter %s.', $name));
+            }
+            if (!is_array($params[$name])) {
+                throw new DomainException(sprintf('Parameter %s must be an array.', $name));
+            }
+
+            /** @phpstan-var array<non-empty-string, array<non-empty-string, mixed>> $array */
+            $array = $params[$name];
+
+            $block = rtrim($matches['block'], "\n");
+            if (strpos($block, '%for') !== false) {
+                throw new DomainException('Nested %for is not supported.');
+            }
+
+            $replaced = [];
+            foreach ($array as $row) {
+                $replaced[] = $this->placeholder_replacer->processQuery($pdo, $block, $row, $bind_values);
+            }
+
+            return implode($glue, $replaced);
+        }, $sql);
+
+        assert($built_sql !== null);
+
+        return $built_sql;
+    }
+}
---
+++ tests/Processor/ForBlockTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Teto\SQL\Processor;
+
+use DomainException;
+use OutOfRangeException;
+use Teto\SQL\QueryBuilder;
+use Teto\SQL\DummyPDO;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectException;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectPHPException;
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+
+/**
+ * @author    USAMI Kenta <tadsan@zonu.me>
+ * @copyright 2022 Baguette HQ
+ * @license   https://github.com/BaguettePHP/TetoSQL/blob/master/LICENSE MPL-2.0
+ */
+final class ForBlockTest extends TestCase
+{
+    use ExpectException;
+    use ExpectPHPException;
+
+    /** @var ForBlock */
+    private $subject;
+
+    public function set_up()
+    {
+        $placeholder_replacer = new DynamicPlaceholder();
+        $this->subject = new ForBlock($placeholder_replacer);
+    }
+
+    /**
+     * @dataProvider acceptDataProvider
+     * @param string $input
+     * @phpstan-param array<non-empty-string,mixed> $params
+     * @param string $expected
+     * @return void
+     */
+    public function test_accept($input, array $params, $expected)
+    {
+        $pdo = new DummyPDO();
+
+        $bind_values = [];
+        $actual = $this->subject->processQuery($pdo, $input, $params, $bind_values);
+
+        $this->assertSame($expected, $actual);
+        $this->assertSame([], $bind_values);
+    }
+
+    /**
+     * @phpstan-return iterable<array{string, array<non-empty-string, mixed>, string}>
+     */
+    public function acceptDataProvider()
+    {
+        $query_has_if = '%for :arr
+  Then!
+%endif
+Rest';
+
+        return [
+            [
+                'No collection', [], 'No collection',
+            ],
+            [
+                '"%for :arr" in literal', [], '"%for :arr" in literal',
+            ],
+            [
+                '%for :arr
+    :a@string - :b@string
+%endfor',
+                [
+                    ':arr' => [
+                        [':a' => 'A1', ':b' => 'B1'],
+                        [':a' => 'A2', ':b' => 'B2'],
+                        [':a' => 'A3', ':b' => 'B3'],
+                    ]
+                ],
+                '
+    @A1@ - @B1@
+    @A2@ - @B2@
+    @A3@ - @B3@',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider rejeceptDataProvider
+     * @param string $input
+     * @phpstan-param array<non-empty-string,mixed> $params
+     * @phpstan-param array{class: class-string<\Exception>, message: string} $expected
+     * @return void
+     */
+    public function test_raise_exception($input, array $params, array $expected)
+    {
+        $pdo = new DummyPDO();
+
+        $this->expectException($expected['class']);
+        $this->expectExceptionMessage($expected['message']);
+
+        $bind_values = [];
+        $actual = $this->subject->processQuery($pdo, $input, $params, $bind_values);
+    }
+
+    /**
+     * @return iterable<array{string, array<mixed>, array{class: class-string<\Exception>, message: string}}>
+     */
+    public function rejeceptDataProvider()
+    {
+        return [
+            [
+                '%for :arr
+    :a@string - :b@string
+%endfor',
+                [],
+                [
+                    'class' => get_class(new DomainException()),
+                    'message' => 'Must be assigned parameter :arr.',
+                ]
+            ],
+            [
+                '%for :arr
+    :a@string - :b@string
+%endfor',
+                [
+                    ':arr' => [
+                        [':a' => 'A1', ':b' => 'B1'],
+                        [':a' => 'A2'],
+                        [':a' => 'A3', ':b' => 'B3'],
+                    ]
+                ],
+                [
+                    'class' => get_class(new OutOfRangeException),
+                    'message' => 'param ":b" expected but not assigned',
+                ]
+            ],
+        ];
+    }
+}
